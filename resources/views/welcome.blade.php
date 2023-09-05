4136a7a4c2f6cd5c743de71517021f5d3015c8de
a.voluysky <a.voluyskiy@rocont.ru>
2023-09-05 15:56:34 +0300

    шаблон фронта

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,32 +11,34 @@
              $commonTitle = 'template';
              $commonDescription = 'descr';
 
-//                 if (!empty($seo) && !empty($seo->getTitle())) {
-//                      $commonTitle = $seo->getTitle();
-//                  } elseif (View::hasSection('title')) {
-//                      $commonTitle = trim(View::yieldContent('title')) . " | WANNGO - интернет-магазин обуви оптом";
-//                  }
-//                  if (!empty($titlePage) && $titlePage !== 1) {
-//                      $commonTitle .= ' | Страница ' . $titlePage;
-//                  }
-//
-//
-//                  if(!empty($seo) && !empty($seo->getDescription())) {
-//                      $commonDescription = $seo->getDescription();
-//                  } elseif (View::hasSection('description')) {
-//                      $commonDescription = trim(View::yieldContent('description'));
-//                  }
+                 if (!empty($seo) && !empty($seo->getTitle())) {
+                      $commonTitle = $seo->getTitle();
+                  } elseif (View::hasSection('title')) {
+                      $commonTitle = trim(View::yieldContent('title')) . " | WANNGO - интернет-магазин обуви оптом";
+                  }
+                  if (!empty($titlePage) && $titlePage !== 1) {
+                      $commonTitle .= ' | Страница ' . $titlePage;
+                  }
+
+                  if(!empty($seo) && !empty($seo->getDescription())) {
+                      $commonDescription = $seo->getDescription();
+                  } elseif (View::hasSection('description')) {
+                      $commonDescription = trim(View::yieldContent('description'));
+                  }
 
         @endphp
         <title>{{$commonTitle}}</title>
         <meta name="description" content="{{$commonDescription}}">
+
         @vite('resources/css/app.scss')
-        </style>
     </head>
     <body>
         <div class="page">
             @include('components.header')
             @yield('content')
+            @if(!empty($seo) && !empty($seo->seo_description))
+                @include('components.seo.seo', ['seo_title' => $seo->seo_title, 'seo_text' => $seo->seo_description, 'image' => $seo->image])
+            @endif
             @include('components.footer')
         </div>
         @vite(['resources/js/app.js'])
