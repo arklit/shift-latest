a72415eb1eca326170a50b0a4a9da89bfe8493e1
Danil Pechenkov <d.pechenkov@rocont.ru>
2024-01-22 15:03:38 +0300

    Rocont CMS: исправить баг value в Tinymce https://trello.com/c/MEwQDnEc

--- resources/views/admin/fields/tinymce.blade.php
+++ resources/views/admin/fields/tinymce.blade.php
@@ -1,5 +1,5 @@
 @component($typeForm, get_defined_vars())
     <div data-controller="tinymce" class="mce-container" data-language="{{$language ?? 'en'}}">
-        <input name="{{ $attributes["name"] }}" value="{!! $value !!}" @if($attributes["required"] === true) required @endif class="tinymce" id="tinymce-wrapper-{{$id}}" style="min-height: {{ $attributes['height'] }}" />
+        <input name="{{ $attributes["name"] }}" value="{{ $value }}" @if($attributes["required"] === true) required @endif class="tinymce" id="tinymce-wrapper-{{$id}}" style="min-height: {{ $attributes['height'] }}" />
     </div>
 @endcomponent
