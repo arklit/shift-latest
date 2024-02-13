c1e7bd975d3a6e6f99a33d0bba5eaae0f6099fa1
danil <pechenkov39@gmail.com>
2024-02-13 14:18:37 +0300

    Небольшие правки

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -1,6 +1,7 @@
 import './validation/validation';
 import './tree';
 import './mobile-commandbar';
+import './upload-sort';
 import CropperRocont from "./controllers/cropper.js";
 import TinyMceController from "./controllers/tinymce";
 
