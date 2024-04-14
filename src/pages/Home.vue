898a8b47980e5fd54fcdec0f31eb7e439492b243
Misty <artemoutside@gmail.com>
2024-04-14 21:27:43 +0300

    template

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useCounterStore} from "@/stores/counter";
+
+let store = useCounterStore()
+</script>
+
+<template>
+  <main>
+    <div style="display: flex; align-items: center; gap: 30px">
+      <span>{{store.count}}</span>
+      <button @click="store.increment()">counter++</button>
+    </div>
+  </main>
+</template>
