91428bfec45918e7bf6e6fc0038f3739fba22f2a
Danil <pechenkov39@gmail.com>
2024-04-22 18:36:08 +0300

    CMS ROCONT: баг с удалением файлов из uploader https://trello.com/c/fKrLGLvu

---
+++ app/Orchid/Fields/Upload.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Orchid\Fields;
+
+class Upload extends \Orchid\Screen\Fields\Upload
+{
+    protected $view = 'admin.fields.upload';
+}
--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -4,6 +4,8 @@ import './mobile-commandbar';
 import './upload-sort';
 import CropperRocont from "./controllers/cropper.js";
 import TinyMceController from "./controllers/tinymce";
+import UploaderRocont from "./controllers/uploader.js";
 
 application.register("cropperrocont", CropperRocont);
 application.register("tinymce", TinyMceController);
+application.register("uploaderrocont", UploaderRocont);
